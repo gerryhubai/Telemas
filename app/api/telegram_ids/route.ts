76d8b1b40ed03d53492baea4f2eb75ca87c42583
gerryhubai <geremaineemefa@gmail.com>
2024-12-30 10:34:14 +0000

    Update route.ts to show only airdropped bal > 0

--- app/api/telegram_ids/route.ts
+++ app/api/telegram_ids/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
     // Query to fetch all telegram IDs
     const result = await sql`
       SELECT telegram_id
-      FROM users
+      FROM users WHERE airdropped_balance > 0;
     `;
 
     // Extract telegram IDs and join them into a comma-separated string
